0d6bb74de9afe5e9623af73156c06c2fbf56ee5c
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-05-24 14:25:55 +0900

    クラスと構造体のコピーの違いを確認

--- Struct/Struct/ViewController.swift
+++ Struct/Struct/ViewController.swift
@@ -20,15 +20,28 @@ struct BoxStruct {
     var color: String = "red"
 }
 
-
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-
+        // クラスの場合
+        let cBox1 = BoxClass()
+        let cBox2 = cBox1
+        cBox2.color = "green"
+        
+        // 構造体の場合
+        let sBox1 = BoxStruct()
+        var sBox2 = sBox1
+        sBox2.color = "green"
+        
+        // 値の出力
+        print("cBox1:\(cBox1.size) \(cBox1.color)")
+        print("cBox2:\(cBox2.size) \(cBox2.color)")
+        print("sBox1:\(sBox1.size) \(sBox1.color)")
+        print("sBox2:\(sBox2.size) \(sBox2.color)")
+        
         
     }
 
