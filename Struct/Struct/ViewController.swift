e3e50a27edf7314492ec3ab7455889d0d29e0c0d
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-05-24 14:16:08 +0900

    クラスと構造体の定義

--- Struct/Struct/ViewController.swift
+++ Struct/Struct/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-struct Ball {
-    static let madein = "日本"
-    static var sozai = "紙"
-    var radius: Double = 10.0
+// クラス
+class BoxClass {
+    var size: String = "M"
+    var color: String = "red"
 }
+
+// 構造体
+struct BoxStruct {
+    var size: String = "M"
+    var color: String = "red"
+}
+
+
 
 class ViewController: UIViewController {
 
@@ -20,13 +28,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        Ball.sozai = "木"
-        let ball1 = Ball(radius: 15)
-        let ball2 = Ball()
-        print(ball1.radius)
-        print(ball2.radius)
-        print(Ball.madein)
-        print(Ball.sozai)
+
         
     }
 
