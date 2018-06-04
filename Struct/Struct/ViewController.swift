5d1211e70dc95c0bce1128e21860678fb80c6be9
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-06-04 22:09:52 +0900

    サブスクリプトでアクセスできる構造体

--- Struct/Struct/ViewController.swift
+++ Struct/Struct/ViewController.swift
@@ -8,57 +8,46 @@
 
 import UIKit
 
-struct Line {
-    // 両端の座標
-    var p1: Point
-    var p2: Point
-    
-    // 両端を動かして線を並行移動する
-    mutating func move(h: Double, v: Double) {
-        p1.x += h
-        p1.y += v
-        p2.x += h
-        p2.y += v
+struct Stock {
+    var name: String
+    var data: [String: Int] = [:]
+    // イニシャライザ
+    init(name: String) {
+        self.name = name
     }
-}
-
-// 点の構造体
-struct Point {
-    var x: Double
-    var y: Double
+    // サブスクリプト
+    subscript(color: String, size: Double) -> Int {
+        //値の取り出し
+        get {
+            let key = color + String(size)
+            if let value = data[key] {
+                return value
+            } else {
+                return 0
+            }
+        }
+        // 値の設定
+        set {
+            let key = color + String(size)
+            data[key] = newValue
+        }
+    }
 }
 
 
 class ViewController: UIViewController {
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // 両端の2点
-        let point1 = Point(x: 100, y: 100)
-        let point2 = Point(x: 300, y: 200)
-        // 2点を結ぶ直線
-        var theLine = Line(p1: point1, p2: point2)
-        // 座標の確認
-        print("移動前のp1\(theLine.p1)")
-        print("移動前のp2\(theLine.p2)")
-        print("-----------")
+    
         
-        // 平行移動する
-        theLine.move(h: 50, v: 60)
-        // 座標の確認
-        print("移動後のp1\(theLine.p1)")
-        print("移動後のp2\(theLine.p2)")
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
 
